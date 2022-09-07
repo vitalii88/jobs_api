32cd24324dadc1ec7e9336aff5893814fb062c85
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-09-06 22:14:17 -0400

    feat(server): init method in auth and jod controllers;

---
+++ server/controllers/auth.js
@@ -0,0 +1,9 @@
+export const register = async (req, resp) => {
+  resp.send('Register user')
+};
+
+export const login = async (req, resp) => {
+  resp.send('Login user')
+};
+
+
---
+++ server/controllers/jobs.js
@@ -0,0 +1,17 @@
+export const getAllGobs = async (req, resp) => {
+  resp.send('getAllJobs controller')
+};
+
+export const getJod = async (req, resp) => {
+  resp.send('getJod controller')
+};
+export const createJob = async (req, resp) => {
+  resp.send('createJob controller')
+};
+export const updateJob = async (req, resp) => {
+  resp.send('updateJob controller')
+};
+export const deleteJod = async (req, resp) => {
+  resp.send('deleteJod controller')
+};
+
