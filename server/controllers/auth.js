ac8724e5cf646138b7301c8685a9cbd27ca1ed35
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-09-10 19:39:54 -0400

    feat(server): add customs error

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -17,9 +17,9 @@ export const register = async (req, resp) => {
 export const login = async (req, resp) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    throw new BadRequestError('Please provide email or password');
-  }
+  // if (!email || !password) {
+  //   throw new BadRequestError('Please provide email or password');
+  // }
   const user = await UserSchema.findOne({ email });
 
   if (!user) {
--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,12 +1,24 @@
-import CustomApiError from '../errors/CustomApiError.js';
+// import CustomApiError from '../errors/CustomApiError.js';
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, resp, next) => {
-  if (err instanceof CustomApiError) {
-    return resp.status(err.statusCode).json({ msg: err.message });
+
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later',
   }
 
-  return resp.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered ${Object.keys(err.keyValue)  } field, please enter original value`;
+    customError.statusCode = 400;
+  }
+
+  if (err.name === 'CustError') {
+    customError.msg = `No item find with id ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  return resp.status(customError.statusCode).json({ msg: customError.msg });
 }
 
 export default errorHandlerMiddleware;
